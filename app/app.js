54ea6f10b57d034269d4c546a7290fd2b99747ad
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-02-28 16:00:34 +0900

    app.js 파일에서 user router 미들웨어를 사용합니다.

--- app/app.js
+++ app/app.js
@@ -1,17 +1,21 @@
-import express from 'express'
-import morgan from 'morgan'
-import { saveConfig } from './src/config/log.js'
+import express from 'express';
+import morgan from 'morgan';
+import { saveConfig } from './src/config/log.js';
+import userRouter from "./src/router/user.js";
+// import { totalTest } from "./src/unit_test/user.js";
 
-const port = 8080
-const app = express()
-const logger = morgan("dev", { stream: saveConfig })
+const port = 8080;
+const app = express();
+const logger = morgan("dev", { stream: saveConfig });
 
 app.use(logger);
-
+app.use("/user", userRouter);
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+    res.send('Hello, World!');
+});
 
 app.listen(port, () => {
-    console.log(`${port}포트에서 서버가 실행되고 있습니다. ${"http://localhost"}:${port}`)
-})
+    console.log(`${port}포트에서 서버가 실행되고 있습니다. ${"http://localhost"}:${port}`);
+});
+
+// await totalTest().then(test_result => console.log(test_result));
