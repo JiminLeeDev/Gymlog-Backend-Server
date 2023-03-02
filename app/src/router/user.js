35caa3083fa1eacd541b701e9bd3e17dd32c8f61
jiminleedev0220 <jiminleedev0220@gmail.com>
2023-03-02 22:38:35 +0900

    where 조건을 쿼리 값으로 받고 조건이 1개 미만일 때 에러를 전송하도록 수정했습니다.

--- app/src/router/user.js
+++ app/src/router/user.js
@@ -1,38 +1,58 @@
 import { Router } from 'express';
+import db from "../config/db.js";
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    if (!req.query.error_test) {
-        res.send({ msg: "The bird flies everytime." });
-    } else {
-        res.status(400).send({ msg: "The bird doesn't fly anymore." });
-    }
+    const id = ["id", req.query.id];
+    const nickname = ["nickname", req.query.nickname];
+    const password = ["password", req.query.password];
+    const query_datas = [id, nickname, password];
+
+    let where = "WHERE";
+
+    query_datas.map((query_data) => where += query_data[1] ? ` ${query_data[0]}="${query_data[1]}" AND` : "");
+
+    where = where === "WHERE" ? "" : where.substring(0, where.length - 4);
+
+    const query = `SELECT * FROM User ${where}`;
+
+    db.query(query, function (error, results, fields) {
+        if (!error) {
+            if (where) {
+                res.send({ msg: results });
+            } else {
+                res.status(400).send({ msg: "적어도 한개 이상의 검색 조건이 주어져야 합니다." });
+            }
+        } else {
+            res.status(400).send({ msg: error });
+        }
+    });
 });
 
-router.post('/', (req, res) => {
-    if (!req.query.error_test) {
-        res.send({ msg: "The bird flies everytime." });
-    } else {
-        res.status(400).send({ msg: "The bird doesn't fly anymore." });
-    }
-});
+// router.post('/', (req, res) => {
+//     if (!req.query.error_test) {
+//         res.send({ msg: "The bird flies everytime." });
+//     } else {
+//         res.status(400).send({ msg: "The bird doesn't fly anymore." });
+//     }
+// });
 
-router.put('/', (req, res) => {
-    if (!req.query.error_test) {
-        res.send({ msg: "The bird flies everytime." });
-    } else {
-        res.status(400).send({ msg: "The bird doesn't fly anymore." });
-    }
-});
+// router.put('/', (req, res) => {
+//     if (!req.query.error_test) {
+//         res.send({ msg: "The bird flies everytime." });
+//     } else {
+//         res.status(400).send({ msg: "The bird doesn't fly anymore." });
+//     }
+// });
 
-router.delete('/', (req, res) => {
-    if (!req.query.error_test) {
-        res.send({ msg: "The bird flies everytime." });
-    } else {
-        res.status(400).send({ msg: "The bird doesn't fly anymore." });
-    }
-});
+// router.delete('/', (req, res) => {
+//     if (!req.query.error_test) {
+//         res.send({ msg: "The bird flies everytime." });
+//     } else {
+//         res.status(400).send({ msg: "The bird doesn't fly anymore." });
+//     }
+// });
 
 
 export default router;
