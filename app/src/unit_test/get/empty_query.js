7ae8f3a10207be7580ad32ed1d79d68feb64dfd5
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-03-06 19:33:07 +0900

    response의 msg에 따라서 unit test의 성공여부가 갈리도록 수정했습니다.

--- app/src/unit_test/get/empty_query.js
+++ app/src/unit_test/get/empty_query.js
@@ -4,16 +4,16 @@ export default async function emptyQueryTest() {
     const emptyQueryTestResult = await new Promise((resolve, reject) => {
         request({
             uri: "http://localhost:8080/user", qs: {
-                id: "a",
-                password: "b",
-                nickname: "c",
+                id: "",
+                password: "",
+                nickname: "",
             }
         }, (err, response, body) => {
             const test_name = "emptyQueryTest";
             const statusCode = response.statusCode;
             const status_text = response.statusMessage;
-            const success = response.statusCode >= 200 && response.statusCode < 300;
             const msg = JSON.parse(body).msg;
+            const success = msg === "검색을 하기 위해 적어도 한 개 이상의 컬럼 값이 필요합니다.";
             const result = { test_name, success, statusCode, status_text, msg };
 
             if (!err) {
