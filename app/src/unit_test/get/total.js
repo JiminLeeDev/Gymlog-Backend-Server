4b41f6bd84b1a9ce213e64fcd9631d0567d8aee4
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-03-06 20:06:00 +0900

    비동기적 호출을 위해 await 키워드를 추가했습니다.

--- app/src/unit_test/get/total.js
+++ app/src/unit_test/get/total.js
@@ -2,5 +2,5 @@ import emptyQueryTest from "./empty_query.js";
 import unExistResult from "./unExistResult.js";
 
 export default async function getTest() {
-    return await [emptyQueryTest(), unExistResult()];
+    return [await emptyQueryTest(), await unExistResult()];
 }
