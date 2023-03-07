b91804a8abb4492dfc5d362c615a1b33d24743a7
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-03-07 15:35:26 +0900

    가독성을 위해 린팅했습니다.

--- app/src/config/log.js
+++ app/src/config/log.js
@@ -1,8 +1,8 @@
-import fs from "fs"
-import path from "path"
+import fs from "fs";
+import path from "path";
 
-const dir = path.resolve()
-const fileName = "api.log"
-const saveConfig = fs.createWriteStream(path.join(dir, fileName), { flags: 'a' })
+const dir = path.resolve();
+const fileName = "api.log";
+const saveConfig = fs.createWriteStream(path.join(dir, fileName), { flags: 'a' });
 
-export { saveConfig }
+export { saveConfig };
