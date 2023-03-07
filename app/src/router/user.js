5bcbb962674e62761c3174c70498552df7b4b7a9
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-03-07 15:34:59 +0900

    user CRUD를 완성했습니다.

--- app/src/router/user.js
+++ app/src/router/user.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import request from 'request';
 import db from "../config/db.js";
 
 const router = Router();
@@ -19,36 +20,74 @@ router.get('/', (req, res) => {
 
     db.query(query, function (error, results, fields) {
         if (!error) {
-            res.send({ msg: results });
+            return res.send({ msg: "요청하신 데이터를 조회했습니다.", error, results, fields });
         } else {
-            res.status(400).send({ msg: error });
+            return res.status(400).send({ msg: "특정할 수 없는 종류의 에러가 발생했습니다.", error, results, fields });
         }
     });
 });
 
-// router.post('/', (req, res) => {
-//     if (!req.query.error_test) {
-//         res.send({ msg: "The bird flies everytime." });
-//     } else {
-//         res.status(400).send({ msg: "The bird doesn't fly anymore." });
-//     }
-// });
+router.post('/', (req, res) => {
+    const id = req.body.id;
+    const nickname = req.body.nickname;
+    const password = req.body.password;
+    const query = `INSERT INTO User(id, nickname, password) VALUES("${id}", "${nickname}", "${password}")`;
 
-// router.put('/', (req, res) => {
-//     if (!req.query.error_test) {
-//         res.send({ msg: "The bird flies everytime." });
-//     } else {
-//         res.status(400).send({ msg: "The bird doesn't fly anymore." });
-//     }
-// });
+    db.query(query, function (error, results, fields) {
+        if (!error) {
+            return res.send({ msg: "요청하신 데이터를 삽입했습니다.", error, results, fields });
+        } else {
+            if (error.code === "ER_DUP_ENTRY") {
+                return res.status(400).send({ msg: "고유 키 값은 중복될 수 없습니다.", error, results, fields });
+            } else {
+                return res.status(400).send({ msg: "특정할 수 없는 종류의 에러가 발생했습니다.", error, results, fields });
+            }
+        }
+    });
+});
 
-// router.delete('/', (req, res) => {
-//     if (!req.query.error_test) {
-//         res.send({ msg: "The bird flies everytime." });
-//     } else {
-//         res.status(400).send({ msg: "The bird doesn't fly anymore." });
-//     }
-// });
+router.put('/', (req, res) => {
+    const id = req.body.id;
+    const nickname = ["nickname", req.body.nickname];
+    const password = ["password", req.body.password];
+    const body_datas = [nickname, password];
+
+    let query = "UPDATE User SET";
+
+    body_datas.map((body_data) => query += body_data[1] ? ` ${body_data[0]} = "${body_data[1]}", ` : "");
+
+    query = query.substring(0, query.length - 2) + `WHERE id = "${id}"`;
+
+    db.query(query, function (error, results, fields) {
+        if (!error) {
+            if (results.affectedRows < 1) {
+                return res.status(400).send({ msg: "요청하신 id에 해당하는 로우 값이 없습니다.", error, results, fields });
+            } else {
+                return res.send({ msg: "요청하신 데이터를 수정했습니다.", error, results, fields });
+            }
+        } else {
+            return res.status(400).send({ msg: "특정할 수 없는 종류의 에러가 발생했습니다.", error, results, fields });
+        }
+    });
+});
+
+router.delete('/', (req, res) => {
+    const id = req.body.id;
+
+    const query = `DELETE FROM User WHERE id="${id}"`;
+
+    db.query(query, function (error, results, fields) {
+        if (!error) {
+            if (results.affectedRows < 1) {
+                return res.status(400).send({ msg: "요청하신 id에 해당하는 로우 값이 없습니다.", error, results, fields });
+            } else {
+                return res.send({ msg: "요청하신 데이터를 삭제했습니다.", error, results, fields });
+            }
+        } else {
+            return res.status(400).send({ msg: "특정할 수 없는 종류의 에러가 발생했습니다.", error, results, fields });
+        }
+    });
+});
 
 
-export default router;
+export default router;;;
