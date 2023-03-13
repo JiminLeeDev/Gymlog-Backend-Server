a9b78c300d070484cb170713a106942d7e3084d6
jiminleedev <jiminleedev0220@gmail.com>
2023-03-13 07:39:56 +0000

    cors 미들웨어를 express 모듈에 추가했습니다.

--- app/app.js
+++ app/app.js
@@ -1,24 +1,28 @@
-import express from 'express';
-import morgan from 'morgan';
-import { saveConfig } from './src/config/log.js';
+import express from "express";
+import morgan from "morgan";
+import { saveConfig } from "./src/config/log.js";
 import userRouter from "./src/router/user.js";
 import threadRouter from "./src/router/thread.js";
 import commentRouter from "./src/router/comments.js";
+import cors from "cors";
 
 const port = 8080;
 const app = express();
 const logger = morgan("dev", { stream: saveConfig });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(logger);
+app.use(cors({ origin: true }));
 app.use("/user", userRouter);
 app.use("/thread", threadRouter);
 app.use("/comment", commentRouter);
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 app.listen(port, () => {
-    console.log(`${port}포트에서 서버가 실행되고 있습니다. ${"http://localhost"}:${port}`);
+  console.log(
+    `${port}포트에서 서버가 실행되고 있습니다. ${"http://localhost"}:${port}`
+  );
 });
